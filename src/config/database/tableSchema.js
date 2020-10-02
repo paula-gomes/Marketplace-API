026172abedc9e7908c182cc3f04205b802e6e61f
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-10-02 18:16:47 -0300

    table creation queries file

---
+++ src/config/database/tableSchema.js
@@ -0,0 +1,46 @@
+module.exports = {
+  USERS: `
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY,
+      name VARCHAR(80) NOT NULL,
+      email VARCHAR(120) NOT NULL,
+      pwrd TEXT NOT NULL,
+      cpf CHAR(9) NOT NULL,
+      phone VARCHAR(9),
+      address TEXT NOT NULL
+    )
+  `,
+
+  SUPPLIERS: `
+  CREATE TABLE IF NOT EXISTS suppliers (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(120) NOT NULL,
+    phone VARCHAR(9) NOT NULL
+  )
+`,
+
+  PRODUCTS: `
+    CREATE TABLE IF NOT EXISTS products (
+      id INTEGER PRIMARY KEY,
+      rating DECIMAL(2,2) NOT NULL
+        CHECK(rating < 10.01),
+      name VARCHAR(120) NOT NULL,
+      description TEXT NOT NULL,
+      price DECIMAL(6,2) NOT NULL,
+      in_stock INTEGER NOT NULL
+        DEFAULT 0,
+      supplier_id INTEGER,
+      FOREIGN KEY(supplier_id) REFERENCES suppliers(id)
+    )
+  `,
+
+  SALES: `
+    CREATE TABLE IF NOT EXISTS sales (
+      id INT PRIMARY KEY,
+      product_id INT,
+      user_id INT,
+      FOREIGN KEY(product_id) REFERENCES products(id),
+      FOREIGN KEY(user_id) REFERENCES users(id)
+    )
+  `,
+};
