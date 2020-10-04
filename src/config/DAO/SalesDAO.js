ab1d701489ee2a03c799410aa2f0e94d715c2773
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-10-04 15:36:45 -0300

    application & DB | sales interaction

--- src/config/DAO/SalesDAO.js
+++ src/config/DAO/SalesDAO.js
@@ -14,36 +14,86 @@ class SalesDAO {
     return new Promise((resolve,reject) => {
       this._db.all(QUERY, (err,res) => {
         if (err)
-          return reject(`Unable to list all sales \n Error: ${err}`);
+          return reject(`Unable to list sales \n Error: ${err}`);
 
         return resolve(res);
       })
     })
   }
 
-  getUser(id) {
+  getUser(user) {
     const QUERY = `
       SELECT
         *
       FROM
         sales
       WHERE
-        id = ?
+        user_id = ?;
     `;
 
     return new Promise((resolve,reject) => {
-      this._db.all(QUERY, [id], (err,res) => {
+      this._db.all(QUERY, user, (err,res) => {
         if (err)
-          return reject(`Unable to list all sales \n Error: ${err}`);
+          return reject(`Unable to list purchases. \n Error: ${err}`);
 
         return resolve(res);
       })
     })
   }
 
-  getProduct() {}
+  getProduct(product) {
+    const QUERY = `
+      SELECT
+        *
+      FROM
+        sales
+      WHERE
+        product_id = ?;
+    `;
 
-  post() {}
+    return new Promise((resolve,reject) => {
+      this._db.all(QUERY, product, (err,res) => {
+        if (err)
+          return reject(`Unable to list sales. \n Error: ${err}`);
 
-  delete() {}
+        return resolve(res);
+      })
+    })
+  }
+
+  post(purchase) {
+    const QUERY = `
+      INSERT INTO
+        sales (user_id,product_id)
+      VALUES
+        ( ? , ? );
+    `;
+
+    return new Promise((resolve,reject) => {
+      this._db.run(QUERY, purchase, err => {
+        if (err)
+          return reject(`Unable to complete the sale. \n Error: ${err}`);
+
+        return resolve('Sale successfull!');
+      })
+    })
+  }
+
+  delete(sale) {
+    const QUERY = `
+      DELETE FROM
+        sales
+      WHERE
+        id = ?;
+    `;
+
+    return new Promise((resolve,reject) => {
+      this._db.run(QUERY, sale, err => {
+        if (err)
+          return reject(`Unable to cancel sale. \n Error: ${err}`);
+
+        return resolve('Sale canceled');
+      })
+    })
+  }
 }
