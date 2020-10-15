391b38128493fcd86198ebce6c5f8ea9287664b0
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-10-15 12:42:26 -0300

    conflict resolved

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const db = require('./config/database/database');
 const clientsRoutes = require('./app/routes/clients.routes');
 const productsRoutes = require('./app/routes/products.routes');
 const salesRoutes = require('./app/routes/sales.routes');
-const providersRoutes = require('./src/app/routes/providers.routes');
+const suppliersRoutes = require('./app/routes/suppliers.routes');
 
 clientsRoutes(app);
 productsRoutes(app);
